c3a45b0bfeea60c83e771f92d522db83a2261efd
monk1982 <hoangthuc2013@gmail.com>
2025-08-04 22:40:33 +0700

    modified:   frontend/main.js

--- frontend/main.js
+++ frontend/main.js
@@ -17,7 +17,7 @@ function getMobileOS(){
 }
 
 let currentFacingMode = "user";
-start_CAM(currentFacingMode)
+//start_CAM(currentFacingMode)
 
 function takePicture() {
     let context = canvas.getContext('2d');
